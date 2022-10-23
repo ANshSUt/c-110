2045221a3fccd840867d1bb471b1ed54f26b27a8
ANshSUt <116484971+ANshSUt@users.noreply.github.com>
2022-10-23 14:21:00 +0530

    Add files via upload

---
+++ roll_a_dice.py
@@ -0,0 +1,13 @@
+from asyncore import loop
+import os
+import shutil
+import random
+
+loop =="y"
+no = random.randint(1,6)
+if no == 1:
+print("[]")
+print("[ ]")
+print("[ 0 ]")
+print("[ ]")
+print("[-----]")
